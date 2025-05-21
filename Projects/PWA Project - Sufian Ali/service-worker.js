6a27dec7c3f720e36a4dc0cbd637f9302e099210
Sufian Ali <71293654+Sufian000@users.noreply.github.com>
2025-05-20 21:30:18 -0400

    Add files via upload

---
+++ Projects/PWA Project - Sufian Ali/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('inventors-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/data.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
